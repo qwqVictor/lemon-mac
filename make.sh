b071fbd9f06efb95fa8f79da8aa78af3f0939877
Victor Huang <i@imvictor.tech>
2018-09-15 15:38:21 +0800

    Made some patch about memory, seemed not working

--- make.sh
+++ make.sh
@@ -3,10 +3,12 @@ echo "Qt 4 enviroment needed, press any key to start making..."
 read -sn 1
 cd "$(dirname "$0")"
 cd project-lemon
-command -v qmake >/dev/null 2>&1 || { (brew tap cartr/qt4 && brew tap-pin cartr/qt4 && brew install qt@4) 2>&1 || { echo >&2 "Require brew but it's not installed.  Aborting."; exit 1; } }
+command -v qmake >/dev/null 2>&1 || { (brew tap cartr/qt4 && brew tap-pin cartr/qt4 && brew install qt@4) 2>&1 || { echo >&2 "Requiring brew but it's not installed.  Aborting."; exit 1; } }
 command -v rpl >/dev/null 2>&1 || { brew install rpl; }
 rpl "Q_OS_LINUX" "Q_OS_MAC" ./*
+rpl "memoryLimit *= 1024 * 1024;" "memoryLimit *= 1024;" ./watcher_unix.c # macOS memory limit fix
 rpl "(usage.ru_maxrss) * 1024" "(usage.ru_maxrss)" ./watcher_unix.c # macOS memory usage fix
+rpl "1024 * 1024" "1024" ./judgingthread.cpp # macOS memory limit fix
 rm -f watcher_unix
 gcc -o watcher_unix watcher_unix.c
 echo >> lemon.pro
@@ -29,7 +31,8 @@ macdeployqt lemon.app
 echo "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPCFET0NUWVBFIHBsaXN0IFBVQkxJQyAiLS8vQXBwbGUvL0RURCBQTElTVCAxLjAvL0VOIiAiaHR0cDovL3d3dy5hcHBsZS5jb20vRFREcy9Qcm9wZXJ0eUxpc3QtMS4wLmR0ZCI+CjxwbGlzdCB2ZXJzaW9uPSIxLjAiPgo8ZGljdD4KCTxrZXk+Q0ZCdW5kbGVFeGVjdXRhYmxlPC9rZXk+Cgk8c3RyaW5nPmxlbW9uPC9zdHJpbmc+Cgk8a2V5PkNGQnVuZGxlR2V0SW5mb1N0cmluZzwva2V5PgoJPHN0cmluZz5MZW1vbiwg566A5piTT0nnq57otZvmtYvor5Xnjq/looM8L3N0cmluZz4KCTxrZXk+Q0ZCdW5kbGVJY29uRmlsZTwva2V5PgoJPHN0cmluZz5sZW1vbjwvc3RyaW5nPgoJPGtleT5DRkJ1bmRsZUlkZW50aWZpZXI8L2tleT4KCTxzdHJpbmc+b3JnLmx5b2kubGVtb248L3N0cmluZz4KCTxrZXk+Q0ZCdW5kbGVQYWNrYWdlVHlwZTwva2V5PgoJPHN0cmluZz5BUFBMPC9zdHJpbmc+Cgk8a2V5PkNGQnVuZGxlU2lnbmF0dXJlPC9rZXk+Cgk8c3RyaW5nPj8/Pz88L3N0cmluZz4KCTxrZXk+Tk9URTwva2V5PgoJPHN0cmluZz5QYXRjaGVkIGJ5IFZpY3RvciBIdWFuZyAmbHQ7aUBpbXZpY3Rvci50ZWNoJmd0OyBpbiBMWU9JPC9zdHJpbmc+Cgk8a2V5Pk5TUHJpbmNpcGFsQ2xhc3M8L2tleT4KCTxzdHJpbmc+TlNBcHBsaWNhdGlvbjwvc3RyaW5nPgo8L2RpY3Q+CjwvcGxpc3Q+Cg==" | base64 -d > lemon.app/Contents/Info.plist
 mkdir /tmp/lemon_make/
 cp -a lemon.app /tmp/lemon_make/
-echo "Done."
+printf '\a\n'
+echo "Done. Press any key to locate it."
 read -sn 1
 open /tmp/lemon_make/
 exit 0
